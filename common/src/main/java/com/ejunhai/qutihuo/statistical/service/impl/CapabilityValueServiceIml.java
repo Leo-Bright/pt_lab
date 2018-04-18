8c175cafb76d12a0653da08d8ba481bf949dc1d9
Leo <buxingzhexiong@sina.com>
2018-04-18 19:37:26 +0800

    能力评定6.2功能完成100%; 但是感觉有业务逻辑的bug，似乎不符合6.2的计算要求。

--- common/src/main/java/com/ejunhai/qutihuo/statistical/service/impl/CapabilityValueServiceIml.java
+++ common/src/main/java/com/ejunhai/qutihuo/statistical/service/impl/CapabilityValueServiceIml.java
@@ -1,12 +1,15 @@
 package com.ejunhai.qutihuo.statistical.service.impl;
 
+import com.ejunhai.qutihuo.statistical.dao.CapabilityEvaluationDataMapper;
 import com.ejunhai.qutihuo.statistical.dao.CapabilityEvaluationMapper;
 import com.ejunhai.qutihuo.statistical.dao.CapabilityValuesMapper;
 import com.ejunhai.qutihuo.statistical.dto.CheckUncertainty;
 import com.ejunhai.qutihuo.statistical.model.CapabilityEvaluation;
+import com.ejunhai.qutihuo.statistical.model.CapabilityEvaluationData;
 import com.ejunhai.qutihuo.statistical.model.CapabilityValue;
 import com.ejunhai.qutihuo.statistical.service.CapabilityEvaluationService;
 import com.ejunhai.qutihuo.statistical.service.CapabilityValueService;
+import com.ejunhai.qutihuo.statistical.utils.CapacityEvaluation;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -20,6 +23,9 @@ public class CapabilityValueServiceIml implements CapabilityValueService {
 
     @Resource
     private CapabilityValuesMapper capabilityValuesMapper;
+
+    @Resource
+    private CapabilityEvaluationService capabilityEvaluationService;
 
     @Override
     public List<CapabilityValue> getCapabilityValuesById(Integer ceId){
@@ -62,9 +68,35 @@ public class CapabilityValueServiceIml implements CapabilityValueService {
                 oneObject.add(str);
                 result.add(oneObject);
             }
-            resultMap.put("th",th);
-            resultMap.put("result",result);
+        }else if("zvalue".equals(method)){
+            CapacityEvaluation capacityEvaluation = new CapacityEvaluation();
+            String[][] capabilityEvaluationDataMatrix = capabilityEvaluationService.getCapabilityEvaluationData(id,1);
+            String[] capabilityEvaluationDataArray = capabilityEvaluationDataMatrix[0];
+            int len = capabilityEvaluationDataArray.length;
+            double[] array = new double[len-5];
+            for(int i=5;i<len;i++){
+                array[i-5] = Double.valueOf(capabilityEvaluationDataArray[i]);
+            }
+            th.add("样品编号");
+            th.add("样品名称");
+            th.add("被测量");
+            th.add("Z值");
+            th.add("评定结果");
+            for(CapabilityValue value:capabilityValues){
+                List<String> oneObject = new ArrayList<>();
+                Map<String,Object> zvalue = capacityEvaluation.zvalue(array,Double.valueOf(value.getxPt()),Double.valueOf(value.getStdPt()));
+                oneObject.add(value.getSampleNo());
+                oneObject.add(value.getSampleName());
+                oneObject.add(value.getVariable());
+                double[] tmpD = (double[])zvalue.get("Z");
+                oneObject.add(String.valueOf(tmpD[0]));
+                String[] tmp = (String[])zvalue.get("result");
+                oneObject.add(tmp[0]);
+                result.add(oneObject);
+            }
         }
+        resultMap.put("th",th);
+        resultMap.put("result",result);
         return resultMap;
     }
 
